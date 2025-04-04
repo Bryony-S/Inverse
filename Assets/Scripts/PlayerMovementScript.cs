182312f53b872133aac9e3f5c67bebce58bdff09
Bryony <187158184+Bryony-S@users.noreply.github.com>
2025-04-04 23:24:36 +0100

    Implemented player movement
    Implemented player grid-based four-directional movement.

---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    // Properties
+    [SerializeField] private float movementSpeed;
+
+    private Vector2 destination;
+    private Vector2 currentDirection = Vector2.zero;
+
+    #region METHODS
+    private void Start()
+    {
+        destination = transform.position;
+    }
+
+    private void Update()
+    {
+        // Player movement
+        if ((Vector2)transform.position != destination)
+        {
+            // Player moves towards destination
+            transform.position = Vector2.MoveTowards(transform.position, destination, Time.deltaTime * movementSpeed);
+        }
+        else if (currentDirection != Vector2.zero)
+        {
+            // Set new destination if current direction is not zero
+            destination += currentDirection;
+        }
+
+    }
+
+    /// <summary>
+    /// Player moves
+    /// </summary>
+    /// <param name="context">Player's input</param>
+    public void Movement(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            currentDirection = context.ReadValue<Vector2>();
+            if (currentDirection.x != 0f) currentDirection.y = 0f; // Stops diagonal movement and prioritises horizontal movement over vertical
+        }
+        else if (context.canceled) { currentDirection = Vector2.zero; }
+    }
+    #endregion
+}
