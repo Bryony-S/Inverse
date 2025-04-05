f9d755a94effed322da9f9c9b8d9f9fb4ae30105
Bryony <187158184+Bryony-S@users.noreply.github.com>
2025-04-05 23:07:31 +0100

    Player cannot switch if colliding with object

--- Assets/Scripts/Player/PlayerSwitchWorldScript.cs
+++ Assets/Scripts/Player/PlayerSwitchWorldScript.cs
@@ -6,17 +6,29 @@ public class PlayerSwitchWorldScript : MonoBehaviour
     // Properties
     [SerializeField] private GameObject yinWorldManager;
     [SerializeField] private GameObject yangWorldManager;
+    [SerializeField] private LayerMask wallLayerMask;
 
+    // Methods
     /// <summary>
     /// Player switches between the yin and yang worlds
     /// </summary>
     /// <param name="context">Player's input</param>
-    public void SwitchWorld(InputAction.CallbackContext context)
+    public void Switch(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            yinWorldManager.SetActive(!yinWorldManager.activeSelf);
-            yangWorldManager.SetActive(!yangWorldManager.activeSelf);
+            SwitchWorlds();
+            // If player collides with a wall, switch back to original world
+            if (Physics2D.OverlapBox(transform.position, transform.localScale * 0.9f, 0f, wallLayerMask)) SwitchWorlds();
         }
+    }
+
+    /// <summary>
+    /// Switch player between worlds
+    /// </summary>
+    private void SwitchWorlds()
+    {
+        yinWorldManager.SetActive(!yinWorldManager.activeSelf);
+        yangWorldManager.SetActive(!yangWorldManager.activeSelf);
     }
 }
