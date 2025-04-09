8bfb43507617cb54a5f14592dbd8a1dc7ada15ee
Bryony <187158184+Bryony-S@users.noreply.github.com>
2025-04-09 01:30:34 +0100

    Created audio manager
    Created audio manager singleton prefab. Added audio clips to assets. Implemented background music.

---
+++ Assets/Scripts/Game Objects/AudioManagerScript.cs
@@ -0,0 +1,30 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AudioManagerScript : MonoBehaviour
+{
+    // Properties
+    public static AudioManagerScript Instance { get; private set; }
+    [SerializeField] private AudioSource audioSource;
+
+    #region Methods
+    private void Awake()
+    {
+        // Set Instance to this if null, otherwise Instance already exists so destroy this
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else { Instance = this; }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    /// <summary>
+    /// Play a sound
+    /// </summary>
+    public void PlaySound(AudioClip sound)
+    {
+        audioSource.PlayOneShot(sound);
+    }
+    #endregion
+}
