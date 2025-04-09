5897794083344bb8b7d9707a272ad268e85e7a09
Bryony <187158184+Bryony-S@users.noreply.github.com>
2025-04-09 23:35:07 +0100

    Added title screen

---
+++ Assets/Scripts/Title Scene/TitleSceneButtonsScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneButtonsScript : MonoBehaviour
+{
+    /// <summary>
+    /// Load scene by name
+    /// </summary>
+    /// <param name="sceneName">The name of the scene to load</param>
+    public void LoadScene(string sceneName)
+    {
+        if (!string.IsNullOrEmpty(sceneName)) SceneManager.LoadScene(sceneName);
+    }
+}
