6a6c2150c7e118926075113e2b237b55fb4a9230
Bryony <187158184+Bryony-S@users.noreply.github.com>
2025-04-11 00:28:03 +0100

    Changed tutorial text
    Controls tutorial text changes based on whether a gamepad is detected or not

---
+++ Assets/Scripts/Controller Detection/ControllerTutorialTextScript.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+
+public class ControllerTutorialTextScript : MonoBehaviour
+{
+    // Properties
+    [SerializeField] private BoolVariable isControllerDetected;
+    [SerializeField] private TMP_Text controlsText;
+
+    private void Update()
+    {
+        if (isControllerDetected.value)
+        {
+            controlsText.text = "Left joystick or D-pad to move\n\nA (bottom) button to push blocks\n\nX (left) button to switch worlds\n\nRB (right shoulder) button to restart level";
+        }
+        else { controlsText.text = "WASD or Arrow keys to move\n\nSpacebar to push blocks\n\nTab to switch worlds\n\nR to restart level"; }
+    }
+}
